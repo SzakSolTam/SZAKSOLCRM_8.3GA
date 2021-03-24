be7bf82e7a29e56a3430bb6b664b8c123546e282
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-03-24 10:44:35 +0100

    - Refactoring and clean-up Vtiger_Viewer - Introducing override default layout function

--- includes/runtime/Viewer.php
+++ includes/runtime/Viewer.php
@@ -10,23 +10,49 @@
 
 vimport ('~/libraries/Smarty/libs/SmartyBC.class.php');
 
-class Vtiger_Viewer extends SmartyBC {
-
+class Vtiger_Viewer extends SmartyBC
+{
+    /**
+     *
+     */
 	const DEFAULTLAYOUT = 'v7';
+
+    /**
+     *
+     */
 	const DEFAULTTHEME  = 'softed';
+
+    /**
+     * @var string
+     */
 	static $currentLayout;
-	
-	// Turn-it on to analyze the data pushed to templates for the request.
+
+    /**
+     * @var string
+     */
+    static $overrideDefaultLayout;
+
+    /**
+     * Turn-it on to analyze the data pushed to templates for the request.
+     *
+     * @var boolean
+     */
 	protected static $debugViewer = false;
 	
 	/**
-	 * log message into the file if in debug mode.
+	 * Log message into the file if in debug mode.
+     *
 	 * @param type $message
 	 * @param type $delimiter 
 	 */
-	protected function log($message, $delimiter="\n") {
+	protected function log($message, $delimiter = "\n")
+    {
 		static $file = null;
-		if ($file == null) $file = dirname(__FILE__) . '/../../logs/viewer-debug.log';
+
+		if ($file == null) {
+		    $file = dirname(__FILE__) . '/../../logs/viewer-debug.log';
+        }
+
 		if (self::$debugViewer) {
 			file_put_contents($file, $message.$delimiter, FILE_APPEND);
 		}
@@ -36,19 +62,20 @@ class Vtiger_Viewer extends SmartyBC {
 	 * Constructor - Sets the templateDir and compileDir for the Smarty files
 	 * @param <String> - $media Layout/Media name
 	 */
-	function __construct($media='') {
+	function __construct($media='')
+    {
 		parent::__construct();
 
 		$THISDIR = dirname(__FILE__);
 
 		$templatesDir = '';
 		$compileDir = '';
-		if(!empty($media)) {
+		if (!empty($media)) {
 			self::$currentLayout = $media;
 			$templatesDir = $THISDIR . '/../../layouts/'.$media;
 			$compileDir = $THISDIR . '/../../test/templates_c/'.$media;
 		}
-		if(!$templatesDir || !file_exists($templatesDir)) {
+		if (!$templatesDir || !file_exists($templatesDir)) {
 			self::$currentLayout = self::getDefaultLayoutName();
 			$templatesDir = $THISDIR . '/../../layouts/'.self::getDefaultLayoutName();
 			$compileDir = $THISDIR . '/../../test/templates_c/'.self::getDefaultLayoutName();
@@ -79,47 +106,78 @@ class Vtiger_Viewer extends SmartyBC {
 			$this->log("URI: $debugViewerURI, TYPE: " . $_SERVER['REQUEST_METHOD']);
 		}
 	}
-	
-	function safeHtmlFilter($content, $smarty) {
+
+    /**
+     * @param $content
+     * @param $smarty
+     * @return mixed|string|string[]|null
+     */
+	function safeHtmlFilter($content, $smarty)
+    {
 		//return htmlspecialchars($content,ENT_QUOTES,UTF-8);
 		// NOTE: to_html is being used as data-extraction depends on this
 		// We shall improve this as it plays role across the product.
 		return to_html($content);
 	}
 
-	/**
-	 * Function to get the current layout name
-	 * @return <String> - Current layout name if not empty, otherwise Default layout name
-	 */
-	public static function getLayoutName() {
-		if(!empty(self::$currentLayout)) {
+    /**
+     * Function to get the current layout name.
+     *
+     * @return string - Current layout name if not empty, otherwise Default layout name
+     */
+	public static function getLayoutName()
+    {
+		if (!empty(self::$currentLayout)) {
 			return self::$currentLayout;
 		}
+
 		return self::getDefaultLayoutName();
 	}
 
-	/**
-	 * Function to return for default layout name
-	 * @return <String> - Default Layout Name
-	 */
-	public static function getDefaultLayoutName(){
+    /**
+     * Function to return for default layout name.
+     *
+     * @return string - Default Layout Name
+     */
+	public static function getDefaultLayoutName()
+    {
+        if (self::$overrideDefaultLayout !== null) {
+            return self::$overrideDefaultLayout;
+        }
+
         return self::DEFAULTLAYOUT;
 	}
 
-	/**
-	 * Function to get the module specific template path for a given template
-	 * @param <String> $templateName
-	 * @param <String> $moduleName
-	 * @return <String> - Module specific template path if exists, otherwise default template path for the given template name
-	 */
-	public function getTemplatePath($templateName, $moduleName='') {
+    /**
+     * Function to return for default layout name.
+     *
+     * @param $layoutName
+     *
+     * @return string - Default Layout Name
+     */
+    public static function overrideDefaultLayoutName($layoutName)
+    {
+        self::$overrideDefaultLayout = $layoutName;
+    }
+
+    /**
+     * Function to get the module specific template path for a given template.
+     *
+     * @param <String> $templateName
+     * @param string $moduleName
+     *
+     * @return string - Module specific template path if exists, otherwise default template path for the given template name
+     */
+	public function getTemplatePath($templateName, $moduleName = '')
+    {
 		$moduleName = str_replace(':', '/', $moduleName);
 		$completeFilePath = $this->getTemplateDir(0). DIRECTORY_SEPARATOR . "modules/$moduleName/$templateName";
-		if(!empty($moduleName) && file_exists($completeFilePath)) {
+
+		if (!empty($moduleName) && file_exists($completeFilePath)) {
 			return "modules/$moduleName/$templateName";
 		} else {
 			// Fall back lookup on actual module, in case where parent module doesn't contain actual module within in (directory structure)
-			if(strpos($moduleName, '/') > 0) {
+			if (strpos($moduleName, '/') > 0) {
 				$moduleHierarchyParts = explode('/', $moduleName);
 				$actualModuleName = $moduleHierarchyParts[count($moduleHierarchyParts)-1];
 				$baseModuleName = $moduleHierarchyParts[0];
@@ -139,15 +197,20 @@ class Vtiger_Viewer extends SmartyBC {
 			return "modules/Vtiger/$templateName";
 		}
 	}
-	
-	/**
-	 * Function to display/fetch the smarty file contents
-	 * @param <String> $templateName
-	 * @param <String> $moduleName
-	 * @param <Boolean> $fetch
-	 * @return html data
-	 */
-	public function view($templateName, $moduleName='', $fetch=false) {
+
+    /**
+     * Function to display/fetch the smarty file contents.
+     *
+     * @param <String> $templateName
+     * @param <String> $moduleName
+     * @param <Boolean> $fetch
+     *
+     * @return html data
+     *
+     * @throws SmartyException
+     */
+	public function view($templateName, $moduleName='', $fetch=false)
+    {
 		$templatePath = $this->getTemplatePath($templateName, $moduleName);
 		$templateFound = $this->templateExists($templatePath);
 		
@@ -183,25 +246,36 @@ class Vtiger_Viewer extends SmartyBC {
 	}
 
 	/**
-	 * Static function to get the Instance of the Class Object
+	 * Static function to get the Instance of the Class Object.
+     *
 	 * @param <String> $media Layout/Media
+     *
 	 * @return Vtiger_Viewer instance
 	 */
-	static function getInstance($media='') {
-		$instance = new self($media);
-		return $instance;
+	static function getInstance($media = '')
+    {
+        return new self($media);
 	}
-
 }
 
-function vtemplate_path($templateName, $moduleName='') {
+/**
+ * @param $templateName
+ * @param string $moduleName
+ * @return false|mixed
+ */
+function vtemplate_path($templateName, $moduleName='')
+{
 	$viewerInstance = Vtiger_Viewer::getInstance();
 	$args = func_get_args();
 	return call_user_func_array(array($viewerInstance, 'getTemplatePath'), $args);
 }
 
 /**
- * Generated cache friendly resource URL linked with version of Vtiger
+ * Generated cache friendly resource URL linked with version of Vtiger.
+ *
+ * @param $url
+ *
+ * @return mixed|string
  */
 function vresource_url($url) {
     global $vtiger_current_version;
@@ -211,6 +285,11 @@ function vresource_url($url) {
     return $url;
 }
 
-function getPurifiedSmartyParameters($param){
+/**
+ * @param $param
+ * @return string
+ */
+function getPurifiedSmartyParameters($param)
+{
     return htmlentities($_REQUEST[$param]);
 }
